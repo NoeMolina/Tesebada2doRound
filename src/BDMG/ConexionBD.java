5bbc0b416cf4a7f9d510f38e5d1f2a4306faaacd
NMMolina <noeml130302@gmail.com>
2024-08-27 13:19:57 -0700

    se agrego clases para el manejo de la base de datos

--- src/BDMG/ConexionBD.java
+++ src/BDMG/ConexionBD.java
@@ -4,10 +4,33 @@
  */
 package BDMG;
 
-/**
- *
- * @author RICARDO VG LAPTOPS
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+//para realizar la conexion a la base de datos
 public class ConexionBD {
+    static final String CONTROLADOR = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private Connection conexion = null;
+    private String ip;
+    private String nameBD;
+    private String user;
+    private String password;
+    private boolean WC;
+    
+    public ConexionBD(String ip, String nameBD, String user, String  password, boolean WC){
+        this.ip = ip;
+        this.nameBD = nameBD;
+        this.user = user;
+        this.password = password;
+        this.WC = WC;
+    }
+    
+    public void MakeConexion(){
+        
+    }
+    
     
 }
